74a19ee740dd2a67a27920385c27fefc18e262cb
gin <X_SJian@163.com>
2022-12-01 12:09:49 +0800

    sql join下开窗（TUMBLE）

--- src/main/java/com/mine/sink/ToCkTableJoin1111.java
+++ src/main/java/com/mine/sink/ToCkTableJoin1111.java
@@ -32,8 +32,8 @@ public class ToCkTableJoin1111 {
         tableConf.setString("table.exec.mini-batch.allow-latency", "5 s");
         // the maximum number of records can be buffered by each aggregate operator task
         tableConf.setString("table.exec.mini-batch.size", "200");
-//        tableConf.setBoolean("table.exec.emit.early-fire.enabled" , true);
-//        tableConf.setString("table.exec.emit.early-fire.delay","5 s");
+        tableConf.setBoolean("table.exec.emit.early-fire.enabled" , true);
+        tableConf.setString("table.exec.emit.early-fire.delay","5 s");
         tableConf.setString("table.exec.state.ttl", "1 DAY");
 
         // 用户登录表
@@ -70,7 +70,6 @@ public class ToCkTableJoin1111 {
                 "`role_id` INT, " +
                 "`created_date` STRING, " +
                 "`created_time` BIGINT, " +
-//                "`ts` TIMESTAMP(3) METADATA FROM 'timestamp', " +
                 "`ts` as TO_TIMESTAMP_LTZ(created_time, 3), " +
                 "WATERMARK FOR ts AS ts - INTERVAL '5' SECOND" +
             ") WITH (" +
@@ -96,7 +95,6 @@ public class ToCkTableJoin1111 {
 //                "`paid_date` STRING, " +
                 "`notify_date` STRING, " +
                 "`notify_time` BIGINT, " +
-//                "`ts` TIMESTAMP(3) METADATA FROM 'timestamp', " +
                 "`ts` as TO_TIMESTAMP_LTZ(notify_time, 3), " +
                 "WATERMARK FOR ts AS ts - INTERVAL '10' SECOND" +
             ") WITH (" +
@@ -110,165 +108,46 @@ public class ToCkTableJoin1111 {
         );
 
         // 联表
-//        String sql = "SELECT \n" +
-//            "    ul.pid as pid,\n" +
-//            "    ul.login_date as report_date,\n" +
-////            "    ul.window_start,\n" +
-////            "    ul.window_end,\n" +
-//            "    GREATEST(ul.ts, IF(r.ts IS NULL, 0, r.ts), IF(o.ts IS NULL, 0, o.ts)) as time_s,\n" +
-//            "    ul.dau,\n" +
-//            "    r.create_role,\n" +
-//            "    o.sum_amount\n" +
-//            "FROM (SELECT \n" +
-//            "        pid,\n" +
-//            "        login_date,\n" +
-////            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-////            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-//            "        MAX(login_time) as ts,\n" +
-//            "        COUNT(DISTINCT uid) as dau\n" +
-//            "    FROM user_login\n" +
-//            "    GROUP BY pid, login_date) AS ul\n" +
-////            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, login_date) AS ul\n" +
-//            "FULL OUTER JOIN (SELECT \n" +
-//            "        pid,\n" +
-//            "        created_date,\n" +
-////            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-////            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-//            "        MAX(created_time) as ts,\n" +
-//            "        COUNT(DISTINCT role_id) as create_role\n" +
-//            "    FROM role\n" +
-//            "    GROUP BY pid, created_date) AS r\n" +
-////            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, created_date) AS r\n" +
-//            "ON ul.pid = r.pid AND r.created_date = ul.login_date\n" +
-//            "FULL OUTER JOIN (SELECT \n" +
-//            "        pid,\n" +
-//            "        notify_date,\n" +
-////            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-////            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-//            "        MAX(notify_time) as ts,\n" +
-//            "        SUM(paid_money) as sum_amount\n" +
-//            "    FROM orders\n" +
-//            "    GROUP BY pid, notify_date) AS o\n" +
-////            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, notify_date) AS o\n" +
-//            "ON o.pid = ul.pid AND o.notify_date = ul.login_date";
+
         String sql = "SELECT \n" +
             "    ul.pid as pid,\n" +
             "    ul.login_date as report_date,\n" +
-//            "    ul.window_start,\n" +
-//            "    ul.window_end,\n" +
-            "    GREATEST(ul.ts, IF(r.ts IS NULL, 0, r.ts), IF(o.ts IS NULL, 0, o.ts)) as time_s,\n" +
-            "    ul.dau,\n" +
-            "    r.create_role,\n" +
-            "    o.sum_amount\n" +
+            "    TUMBLE_START(ul.ts, INTERVAL '1' DAY) as window_start, \n" +
+            "    TUMBLE_END(ul.ts, INTERVAL '1' DAY) as window_end, \n" +
+            "    SUM(ul.dau) as dau,\n" +
+            "    SUM(r.create_role) as create_role,\n" +
+            "    SUM(o.sum_amount) as sum_amount\n" +
             "FROM (SELECT \n" +
             "        pid,\n" +
             "        login_date,\n" +
-//            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-//            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-            "        MAX(login_time) as ts,\n" +
+            "        MAX(ts) as ts,\n" +
             "        COUNT(DISTINCT uid) as dau\n" +
-//            "    FROM user_login\n" +
-            "    FROM TABLE(\n" +
-            "        CUMULATE( TABLE user_login, DESCRIPTOR(ts), INTERVAL '5' SECOND, INTERVAL '1' DAY )\n" +
-            "    )" +
+            "    FROM user_login\n" +
             "    GROUP BY pid, login_date) AS ul\n" +
-//            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, login_date) AS ul\n" +
             "FULL OUTER JOIN (SELECT \n" +
             "        pid,\n" +
             "        created_date,\n" +
-//            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-//            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-            "        MAX(created_time) as ts,\n" +
+            "        MAX(ts) as ts,\n" +
             "        COUNT(DISTINCT role_id) as create_role\n" +
-//            "    FROM role\n" +
-            "    FROM TABLE(\n" +
-            "        CUMULATE( TABLE role, DESCRIPTOR(ts), INTERVAL '5' SECOND, INTERVAL '1' DAY )\n" +
-            "    )" +
+            "    FROM role\n" +
             "    GROUP BY pid, created_date) AS r\n" +
-//            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, created_date) AS r\n" +
             "ON ul.pid = r.pid AND r.created_date = ul.login_date\n" +
             "FULL OUTER JOIN (SELECT \n" +
             "        pid,\n" +
             "        notify_date,\n" +
-//            "        TUMBLE_START(ts, INTERVAL '1' DAY) as window_start, \n" +
-//            "        TUMBLE_END(ts, INTERVAL '1' DAY) as window_end, \n" +
-            "        MAX(notify_time) as ts,\n" +
+            "        MAX(ts) as ts,\n" +
             "        SUM(paid_money) as sum_amount\n" +
-//            "    FROM orders\n" +
-            "    FROM TABLE(\n" +
-            "        CUMULATE( TABLE orders, DESCRIPTOR(ts), INTERVAL '5' SECOND, INTERVAL '1' DAY )\n" +
-            "    )" +
+            "    FROM orders\n" +
             "    GROUP BY pid, notify_date) AS o\n" +
-//            "    GROUP BY TUMBLE(ts, INTERVAL '1' DAY), pid, notify_date) AS o\n" +
-            "ON o.pid = ul.pid AND o.notify_date = ul.login_date";
+            "ON o.pid = ul.pid AND o.notify_date = ul.login_date\n" +
+            "GROUP BY TUMBLE(ul.ts, INTERVAL '1' DAY), ul.pid, ul.login_date";
         System.out.println(sql);
         Table totalData = tableEnv.sqlQuery(sql);
         tableEnv.toChangelogStream(totalData).print("totalData");
 
-        // 创建联表后暂存的数据表
-//        tableEnv.executeSql("create table total_data (" +
-//                "`pid` INT, " +
-//                //                "`gid` int, " +
-//                //                "`p_mid` int, " +
-//                //                "`mid` int, " +
-//                "`dau` BIGINT, " +
-//                "`create_role` BIGINT, " +
-//                "`sum_amount` DECIMAL(38, 0), " +
-//                //                "`new_sum_amount` decimal, " +
-//                //                "`reg_date` string, " +
-//                "`report_date` STRING, " +
-//                "`time_s` BIGINT, " +
-//                "`ts` as TO_TIMESTAMP_LTZ(time_s, 3), " +
-//                //                "PRIMARY KEY (`pid`, `gid`, `p_mid`, `mid`, `reg_date`, `report_date`) NOT ENFORCED " +
-//                "PRIMARY KEY (`pid`, `report_date`) NOT ENFORCED, " +
-//                "WATERMARK FOR ts AS ts - INTERVAL '5' SECOND" +
-//            ") with ( " +
-//                "'connector' = 'upsert-kafka', " +
-//                "'topic' = 'total-data', " +
-//                "'properties.bootstrap.servers' = '127.0.0.1:9092', " +
-//                "'key.format' = 'json', " +
-//                "'value.format' = 'json'" +
-//            ")");
-//
-//        // 数据入到联表后的暂存表
-//        tableEnv.executeSql(
-//            "INSERT INTO total_data SELECT " +
-//                "pid, " +
-////                "gid, " +
-////                "p_mid, " +
-////                "mid, " +
-//                "dau, " +
-//                "create_role, " +
-//                "sum_amount, " +
-//                "report_date, " +
-//                "time_s " +
-//            "FROM (" + sql + ")");
-//
-//
-//        String cumulateSql = "SELECT \n" +
-//            "    pid, \n" +
-//            "    report_date, \n" +
-////            "    gid, \n" +
-////            "    p_mid, \n" +
-////            "    mid, \n" +
-//            "    SUM(dau) as dau, \n" +
-//            "    SUM(create_role) as create_role, \n" +
-//            "    SUM(sum_amount) as sum_amount, \n" +
-//            "    window_start, \n" +
-//            "    window_end \n" +
-//            "FROM TABLE(\n" +
-//            "    CUMULATE( TABLE total_data, DESCRIPTOR(ts), INTERVAL '5' SECOND, INTERVAL '1' DAY )\n" +
-//            ")" +
-//            "GROUP BY \n" +
-////            "    pid, gid, p_mid, mid, reg_date, login_date, window_start, window_end\n" +
-//            "    pid, report_date, window_start, window_end";
-//        Table operatorData = tableEnv.sqlQuery(cumulateSql);
-//        tableEnv.toChangelogStream(operatorData).print("operatorData");
-
 
 //        tableEnv.executeSql(
-//            "create table operator (" +
-//                "`pid` int, " +
+//            "create table operator (" +//                "`pid` int, " +
 ////                "`gid` int, " +
 ////                "`p_mid` int, " +
 ////                "`mid` int, " +
